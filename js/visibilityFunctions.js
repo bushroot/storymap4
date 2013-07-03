8e4653e53a6fd3667f16f0406a5bbc08ccc14d55
ltbar <ltbar@ip-10-51-55-74.eu-west-1.compute.internal>
2013-07-03 14:09:03 +0200

    temporal save

--- js/visibilityFunctions.js
+++ js/visibilityFunctions.js
@@ -1,21 +1,32 @@
+    
 
 
-// does not work! change variable in css instead!
-function toggleElements(id){
-	document.getElementById(id).style.display = "inline";
-    ide = id.toString(); 
-	for (i=0;i<elements.length;i++){
-		if (elements[i] != ide){
-			document.getElementById(elements[i]).style.display = "hidden";
-		}		
-	}
+
+
+function changeCSS(cssFile, cssLinkIndex) {
+	var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+	
+	var newlink = document.createElement("link")
+	newlink.setAttribute("rel", "stylesheet");
+	newlink.setAttribute("type", "text/css");
+	newlink.setAttribute("href", cssFile);
+	
+	document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
 }
 
 
 
 
-var elements = new Array("overviewMapSpan", "detailMapSpan", "objectInfo", "barChart");
-//	document.getElementById("overviewMapSpan").style.display = "none";
-
-
-
+/*
+// does not work! change variable in css instead!
+var mapArrays = new Array("overviewMapSpan", "detailMapSpan");
+function toggleElements(id){
+	document.getElementById(id).style.width = "100%";
+    ide = id.toString(); 
+	for (i=0;i<elements.length;i++){
+		if (elements[i] != ide){
+			document.getElementById(elements[i]).style.width = "0%";
+		}		
+	}
+}
+*/
