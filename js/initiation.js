413b6b9a9fcba7f7bc5273456e8e3fa5ddb39bad
ltbar <ltbar@ip-10-51-55-74.eu-west-1.compute.internal>
2013-07-16 16:05:16 +0200

    add waning message for IE <= 8

---
+++ js/initiation.js
@@ -0,0 +1,16 @@
+
+function initiation(){
+
+	if ( $.browser.msie && $.browser.version <= 8) {
+		$('div#mainContent').replaceWith(
+			'<div class="browserError">'
+				+'<h1>Incompatible Browser</h1>'
+				+ '<p>Please update your browser or use different modern web browser!</p>'
+			+'</div)');
+	}
+	else{
+		loadOverviewMap();
+		loadDetailMap();
+	}
+
+}
