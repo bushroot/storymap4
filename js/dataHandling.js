2f11ed1c235d185b2d4ff3896c1ad92c489f21d0
ltbar <ltbar@ip-10-51-55-74.eu-west-1.compute.internal>
2013-07-16 09:19:43 +0200

    solve proxy issue for obtaining crossdomain data

--- js/dataHandling.js
+++ js/dataHandling.js
@@ -1,64 +1,12 @@
 
-var data;
+// Proxy for cross domain data access
+// OpenLayers.ProxyHost= "../../../../cgi-bin/proxy.cgi?url=";
 
-$(document).ready(function()
-{
-  $.ajax({
-    type: "GET",
-    url: "data/sms.xml",
-    dataType: "xml",
-    success: storeInVariable 
-  });
-});
-
-function storeInVariable(xml){
-	data = $(xml);
-}
-
-
-function parseXml(xml){
-	$(xml).find("Name").each(function()
-	{
-		$("#xmlData").append($(this).attr("author") + "<br />");
-	});
-}
-
-/*
-var invocation = new XMLHttpRequest();
-var url = "http://www.hydrodaten.admin.ch/lhg/";
- 
-/*  
-function callOtherDomain() {
-  if(invocation) {    
-    invocation.open('GET', url, true);
-    invocation.onreadystatechange = handler;
-    invocation.send(); 
-  }
-}
-
-callOtherDomain();
-*/
-/*
-OpenLayers.ProxyHost= "proxy.cgi?url=";
-
-
-
-/*if (window.XMLHttpRequest)
-  {// code for IE7+, Firefox, Chrome, Opera, Safari
-  xmlhttp=new XMLHttpRequest();
-  }
-else
-  {// code for IE6, IE5
-  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-xmlhttp.open("GET","http://www.hydrodaten.admin.ch/lhg/SMS.xml",false);
-xmlhttp.send();
-xmlDoc=xmlhttp.responseXML;*/
-/*
 var format = new OpenLayers.Format.XML();
 
 OpenLayers.Request.GET({
-	url: "http://www.hydrodaten.admin.ch/lhg/SMS.xml",
+	//url: "http://www.hydrodaten.admin.ch/lhg/SMS.xml",
+	url: "data/sms.xml",
 	success: function(request) {
 		console.log(request);
 		xml = format.read(request.responseText);
@@ -69,4 +17,9 @@ OpenLayers.Request.GET({
 	}
 });
 
-*/
+
+
+
+
+
+
