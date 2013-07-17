9de728d5e6cc0ecf09b1c24e92476e93eb0be641
ltbar <ltbar@ip-10-51-55-74.eu-west-1.compute.internal>
2013-07-17 17:06:53 +0200

    filter temperature records from xml

--- js/dataHandling.js
+++ js/dataHandling.js
@@ -8,14 +8,74 @@ OpenLayers.Request.GET({
 	//url: "http://www.hydrodaten.admin.ch/lhg/SMS.xml",
 	url: "data/sms.xml",
 	success: function(request) {
-		console.log(request);
-		xml = format.read(request.responseText);
+		xml = format.read(request.responseText).documentElement;
 		console.log(xml);
+		filterStations(xml);
 	},
 	failure: function(request) {
-		console.log(request);
+		console.log("XML loading failed!");
 	}
 });
+
+
+//check if the first node is an element node
+function getfirstchild(n)	{
+	x=n.firstChild;
+	while (x.nodeType!=1){
+  		x=x.nextSibling;
+  	}
+	return x;
+}
+
+
+
+function filterStations(xml){
+	
+	var records = xml.getElementsByTagName("MesPar");
+	for (var i =0; i < records.length; i++){
+	
+		// get the all records that measure the water temperature
+		var typvalue = records[i].getAttributeNode("Typ").nodeValue;	
+	
+		// get the required values from the records
+		if (typvalue == 03){
+			var strnr = records[i].getAttributeNode("StrNr").nodeValue;	
+			var temp = records[i].childNodes[7].childNodes[0].nodeValue;
+		}
+		
+			
+
+
+
+	}
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Openlayer Filter !
 
 
 
