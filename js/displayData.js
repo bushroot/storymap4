a2f52182ab41bcc8640acb4afc1a9363645d9bc0
ltbar <ltbar@ip-10-51-55-74.eu-west-1.compute.internal>
2013-07-26 12:04:42 +0200

    start display funciton

--- js/displayData.js
+++ js/displayData.js
@@ -27,19 +27,11 @@
 //********************************************************
 
 
+function displayObjectInfo(){
+	$("#stationName").innerHTML = "test";
 
-function getObjectData(id){
-	var geoJsonData = overviewLayer.getFeaturesByAttribute("edv_nr4", id)[0].data;
-//	var xmlData =  
-	return geoJsonData;
-//	return xmlData;
-	
+
 }
-
-
-
-
-
 
 
 
