6644e94e16e01bd0be18a5a85ff71c5b8e5bc703
ltbar <ltbar@ip-10-51-55-74.eu-west-1.compute.internal>
2013-08-06 09:52:09 +0200

    add multilingual document title

--- js/translate.js
+++ js/translate.js
@@ -4,3 +4,23 @@ i18n.init({lng: lang} , function(t) {
   $(".multilang").i18n();
 });
 
+
+$(document).ready(function(){
+	if (lang == "de"){ 
+		$("title").html("Die wärmsten Fliessgewässer der Schweiz");
+	}
+	if (lang == "fr"){ 
+		$("title").html("Les cours d’eau les “plus chauds“ de Suisse");
+	}
+	if (lang == "it"){ 
+		$("title").html("I fiumi “più caldi” della Svizzera");
+	}
+	if (lang == "en"){ 
+		$("title").html("The warmest rivers of Switzerland");
+	}
+
+
+});
+
+
+
