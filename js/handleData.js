76bb811d09a8a4ceb683b0c3ab5778824e99f354
Roger Baer <ltbar@ip-10-227-105-226.eu-west-1.compute.internal>
2013-08-08 15:01:53 +0200

    solve no-clickable menue on mobiles

--- js/handleData.js
+++ js/handleData.js
@@ -19,14 +19,14 @@ var firstLoad = true;
 
 
 // Proxy for cross domain data access
-// OpenLayers.ProxyHost= "../../../../cgi-bin/proxy.cgi?url=";
+ OpenLayers.ProxyHost= "../../../../cgi-bin/proxy.cgi?url=";
 
 
 function loadXmlData(callback){
 	var format = new OpenLayers.Format.XML();
 	OpenLayers.Request.GET({
-		//url: "http://www.hydrodaten.admin.ch/lhg/SMS.xml",
-		url: "data/sms.xml",
+		url: "http://www.hydrodaten.admin.ch/lhg/SMS.xml",
+		//url: "data/sms.xml",
 		success: function(request) {
 			if(request.responseXML == null) {
     			console.log("its null");
